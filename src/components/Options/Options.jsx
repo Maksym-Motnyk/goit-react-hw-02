8a8cbe50db069d2bdc444e1d838a4d12ba4ab5c8
Maksym Motnyk <maksummotnuk@gmail.com>
2024-07-04 18:41:52 +0300

    Homework-2

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,29 @@
+import css from "./Options.module.css";
+
+export default function Options({
+  updateFeedback,
+  totalFeedback,
+  resetFeedback,
+}) {
+  return (
+    <div className={css.container}>
+      <button onClick={() => updateFeedback("good")} className={css.btn}>
+        Good
+      </button>
+
+      <button onClick={() => updateFeedback("neutral")} className={css.btn}>
+        Neutral
+      </button>
+
+      <button onClick={() => updateFeedback("bad")} className={css.btn}>
+        Bad
+      </button>
+
+      {totalFeedback > 0 && (
+        <button onClick={resetFeedback} className={css.btn}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
